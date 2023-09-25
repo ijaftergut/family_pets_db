9ce6ab63fc13e7506ca8058101c29f3fd24a18e1
Isaac Aftergut <ijagut@vt.edu>
2023-09-25 15:25:37 -0400

    functional final

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/family_pet_db');
 const express = require('express');
 const app = express();
 const path = require('path');
+
+app.use(express.json())
 
 const homePage = path.join(__dirname, 'index.html');
 app.get('/', (req, res)=> res.sendFile(homePage));
@@ -16,15 +18,68 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+app.put('/api/pets/:id', async(req, res, next)=>{
+  try{
+    const SQL = `
+      UPDATE pets SET user_id = $1, name = $2
+      WHERE id = $3 RETURNING *
+    `
+    const response = await client.query(SQL, [req.body.user_id, req.body.name, req.params.id])
+    res.send(response.rows[0]);
+  } catch(error){
+    next(error)
+  }
+})
+
+app.get('/api/pets', async(req, res, next)=> {
+  try {
+    const response = await client.query('SELECT * FROM pets ORDER BY name');
+    res.send(response.rows);
+  }
+  catch(ex){
+    next(ex);
+  }
+});
+app.get('/api/users', async(req,res,next)=>{
+  try{
+    const response = await client.query('SELECT * FROM users');
+    res.send(response.rows);
+
+  } catch(error){
+    next(error)
+  }
+})
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
-  `;
+  DROP TABLE IF EXISTS pets;
+  DROP TABLE IF EXISTS users;
+    CREATE TABLE users(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) UNIQUE
+
+    );
+    CREATE TABLE pets(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) UNIQUE,
+      user_id INTEGER REFERENCES users(id)
+
+    );
+    INSERT INTO users(name) VALUES ('Isaac');
+    INSERT INTO users(name) VALUES ('CJ');
+    INSERT INTO users(name) VALUES ('Karen');
+    INSERT INTO users(name) VALUES ('Jeff');
+    INSERT INTO pets(name, user_id) VALUES ('Molly', (SELECT id FROM users WHERE name=''));
+    INSERT INTO pets(name, user_id) VALUES ('Harriet', (SELECT id FROM users WHERE name=''));
+    INSERT INTO pets(name, user_id) VALUES ('McGinnis', (SELECT id FROM users WHERE name=''));
+    INSERT INTO pets(name, user_id) VALUES ('Mia', (SELECT id FROM users WHERE name=''));
+    `;
+  await client.query(SQL)
   console.log('create your tables and seed data');
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 4000;
   app.listen(port, ()=> {
     console.log(`listening on port ${port}`);
   });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,99 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
-
+import axios from 'axios';
 const App = ()=> {
+  const [users, setUsers] = useState([])
+  const [pets, setPets] = useState([])
+  
+  useEffect(()=>{
+    const fetchUsers = async()=>{
+      const response = await axios.get('/api/users')
+      setUsers(response.data)
+
+    }
+    fetchUsers()
+  }, [])
+  
+  useEffect(()=>{
+    const fetchPets = async()=>{
+      const response = await axios.get('/api/pets')
+      setPets(response.data)
+
+    }
+    fetchPets()
+  }, [])
+  const addOwner = async(pet, user)=>{
+    pet = {
+      ...pet, user_id: user.id
+    }
+    const response = await axios.put(`/api/pets/${pet.id}`, pet)
+    pet = response.data
+  }
+  const removeOwner = async (pet) => {
+    pet = { ...pet, user_id: null };
+    const response = await axios.put(`/api/pets/${pet.id}`, pet); // Update this line
+    pet = response.data;
+    setPets(pets.map((_pet) => (_pet.id === pet.id ? pet : _pet)));
+  };
+  
+
   return (
-    <h1>FullStack Template</h1>
+   <div>
+      <h1> Pet Favoritism Tracker</h1>
+      <main>
+      <div>
+      <h1> Owners ({users.length})</h1>
+      <ul>
+        {
+          users.map(user=>{
+            const usersThings = pets.filter(thing=>
+              thing.user_id === user.id)
+            
+            return(
+              <li key={user.id}>{user.name}({usersThings.length})</li>
+            )
+          })
+        }
+      </ul>
+      </div>
+      <div>
+      <h1> Pets ({pets.length})</h1>
+      <ul>
+        {
+          pets.map(pet=>{
+            return(
+              <li key={pet.id}>{pet.name}
+              <ul>
+                {
+                  users.map( user =>{
+                    return(
+                      <li key={user.id} className={pet.user_id === user.id ? 'owner':''}>{user.name}
+                      {
+                        pet.user_id === user.id ? (
+                        <button onClick={()=> removeOwner(pet)}>
+                        Remove
+                        </button>
+                        ): (
+                          <button onClick={()=> addOwner(pet, user)}>
+                          Add
+                          </button>
+                        )
+                      }
+
+                        </li>
+                    )
+                  })
+                }
+              </ul>
+              </li>
+            )
+          })
+        }
+      </ul>
+      </div>
+      </main>
+
+   </div>
   );
 };
 
